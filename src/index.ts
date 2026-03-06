c7a242beead4971d919f92410ca8bc4d10c52287
Kennito-H <kennyhoh2014@gmail.com>
2026-03-06 15:05:29 -0500

    saving packages to github

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('../config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -11,6 +11,6 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (readyClient: any) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
-
+// console.log(process.env.A);
 // Log in to Discord with your client's token
 client.login(token);
